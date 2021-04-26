379db57fed443c23df926149e44c902f2ddf2712
voyager1 <adilbekk8@gmail.com>
2021-04-26 23:48:37 +0600

    add apply view

--- apps/orders/views.py
+++ apps/orders/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework.generics import CreateAPIView
 
-# Create your views here.
+from apps.common.mixins import PublicAPIMixin, JSONRendererMixin
+
+from .serializers import ApplyLeadSerializer
+from .models import Lead
+
+
+class ApplyView(PublicAPIMixin, JSONRendererMixin, CreateAPIView):
+    serializer_class = ApplyLeadSerializer
+    queryset = Lead.objects.all()
+
+    def perform_create(self, serializer):
+        lead = serializer.save()
+        print("created lead:", lead)
+        # create service to find organization
