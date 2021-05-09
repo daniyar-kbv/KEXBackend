db78683f19346a7b58e92330e458eab182c917af
voyager1 <adilbekk8@gmail.com>
2021-05-10 01:01:13 +0600

    minor changes in iiko organization service

--- apps/pipeline/iiko/integrations/organizations.py
+++ apps/pipeline/iiko/integrations/organizations.py
@@ -2,16 +2,57 @@ from typing import Any
 
 from .base import BaseIIKOService
 
-from .serializers import UpdateBrandSerializer
+from .serializers import IIKOOrganizationSerializer
 
 
 class GetOrganizations(BaseIIKOService):
     """Получение списка организации"""
     endpoint = "api/1/organizations"
-    save_serializer = UpdateBrandSerializer
+    save_serializer = IIKOOrganizationSerializer
 
     def run_service(self) -> Any:
-        return self.fetch(json={})
+        return self.fetch(json={
+                "returnAdditionalInfo": True,
+        })
+
+    @staticmethod
+    def parse_address(full_address=None):
+        try:
+            district, city, street_with_building = full_address.split(', ')
+            street, building = street_with_building.split()
+        except (ValueError, AttributeError):
+            return {}
+
+        return {
+            "city": city,
+            "district": district,
+            "street": street,
+            "building": building,
+        }
+
+    def prepare_to_save(self, data: dict) -> list:
+        prepared_data = list()
+        for org in data["organizations"]:
+            prepared_data.append({
+                "iiko_brand": self.instance.pk,
+                "name": org.get("name"),
+                "outer_id": org.get("id"),
+                "address": {
+                    "country": org.get("country"),
+                    "longitude": org.get("longitude"),
+                    "latitude": org.get("latitude"),
+                    **self.parse_address(org.get("restaurantAddress")),
+                },
+            })
+
+        return prepared_data
 
     def finalize_response(self, response):
         return None
+
+    def save(self, prepared_data):
+        serializer = self.save_serializer(
+            data=self.prepare_to_save(prepared_data), many=True
+        )
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
--- apps/pipeline/iiko/integrations/serializers.py
+++ apps/pipeline/iiko/integrations/serializers.py
@@ -1,35 +1,42 @@
 from rest_framework import serializers
 
+from apps.location.models import Address
 from apps.partners.models import IIKOBrand, Organization
 
 
-class OrganizationSerializer(serializers.ModelSerializer):
-    id = serializers.UUIDField(source="outer_id", required=True)
+class IIKOAddressSerializer(serializers.ModelSerializer):
+    longitude = serializers.CharField()
+    latitude = serializers.CharField()
+
+    class Meta:
+        model = Address
+        fields = "__all__"
+
+
+class IIKOOrganizationSerializer(serializers.ModelSerializer):
+    address = IIKOAddressSerializer(required=False)
 
     class Meta:
         model = Organization
-        fields = (
-            "id",
-            "name",
+        fields = "__all__"
+
+    def create(self, validated_data):
+        address = validated_data.pop("address", None)
+
+        instance, created = Organization.objects.update_or_create(
+            outer_id=validated_data.pop("outer_id"),
+            defaults={
+                "is_active": True,
+                **validated_data,
+            }
         )
+        print("instance", instance)
 
-
-class UpdateBrandSerializer(serializers.ModelSerializer):
-    organizations = OrganizationSerializer(many=True, required=True)
-
-    class Meta:
-        model = IIKOBrand
-        fields = "organizations",
-
-    def update(self, instance, validated_data):
-        for organization_data in validated_data.pop("organizations"):
-            Organization.objects.update_or_create(
-                outer_id=organization_data["outer_id"],
-                defaults={
-                    "is_active": True,
-                    "iiko_brand": instance,
-                    "name": organization_data["name"],
-                },
-            )
+        address_serializer = IIKOAddressSerializer(
+            instance=instance.address,
+            data=address,
+        )
+        address_serializer.is_valid(raise_exception=True)
+        address_serializer.save()
 
         return instance
