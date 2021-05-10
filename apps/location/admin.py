898d7fc85fee7091ad8105f22341021df6960242
voyager1 <adilbekk8@gmail.com>
2021-05-10 17:28:49 +0600

    add IIKOBrandInline to change brand priorities

--- apps/location/admin.py
+++ apps/location/admin.py
@@ -1,6 +1,32 @@
 from django.contrib import admin
 
+from apps.partners.models import IIKOBrand
+
 from .models import Country, City
 
+
+class IIKOBrandInline(admin.TabularInline):
+    model = IIKOBrand
+    readonly_fields = "brand_name",
+    fields = (
+        "brand_name",
+        "priority",
+    )
+
+    def brand_name(self, obj):
+        return obj.brand.name
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    def has_add_permission(self, request, obj):
+        return False
+
+
+class CityAdmin(admin.ModelAdmin):
+    model = City
+    inlines = IIKOBrandInline,
+
+
 admin.site.register(Country)
-admin.site.register(City)
+admin.site.register(City, CityAdmin)
