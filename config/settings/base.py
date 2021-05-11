1b772a6c9a42ccfdd16e8471e76239e9196da351
voyager1 <adilbekk8@gmail.com>
2021-05-12 01:51:08 +0600

    set constance host before installing local apps

--- config/settings/base.py
+++ config/settings/base.py
@@ -2,9 +2,8 @@ import base64
 import os
 from datetime import timedelta
 
-from config.constants.dynamic_constance_configs import CONSTANCE_REDIS_CONNECTION, CONSTANCE_CONFIG, CONSTANCE_CONFIG_FIELDSETS
-
 from . import Languages
+from config.constants.error_messages import ERROR_MESSAGES
 
 
 PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -21,7 +20,27 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
-# Application definition
+CONSTANCE_CONFIG = {
+    "IIKO_SERVICE_HOST": ("https://api-ru.iiko.services", "IIKO service host"),
+    "IIKO_AUTH_TOKEN_LIFETIME": (60 * 30, "IIKO auth token lifetime in seconds"),
+    **ERROR_MESSAGES,
+}
+
+CONSTANCE_CONFIG_FIELDSETS = {
+    "IIKO Credentials&settings": (
+        "IIKO_SERVICE_HOST",
+        "IIKO_AUTH_TOKEN_LIFETIME",
+    ),
+    "Error messages": (
+        tuple(ERROR_MESSAGES.keys())
+    ),
+}
+
+CONSTANCE_REDIS_CONNECTION = {
+    'host': 'redis',
+    'port': 6379,
+    'db': 0,
+}
 
 DJANGO_APPS = [
     'django.contrib.admin',
