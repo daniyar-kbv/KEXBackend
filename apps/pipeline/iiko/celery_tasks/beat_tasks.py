7e7f0bf764a350783ad9451b19b9d065116b0a5b
voyager1 <adilbekk8@gmail.com>
2021-05-24 01:07:35 +0600

    add beat task for update brand nomenclatures

--- apps/pipeline/iiko/celery_tasks/beat_tasks.py
+++ apps/pipeline/iiko/celery_tasks/beat_tasks.py
@@ -5,13 +5,30 @@ Only for cron, not for import
 from config import celery_app
 from django.db.transaction import atomic
 
-from apps.partners.models import IIKOBrand
+from apps.partners.models import IIKOBrand, Organization
 from ..integrations.organizations import GetOrganizations
+from ..integrations.nomenclature import GetOrganizationNomenclature
 
 
 @celery_app.task(name="iiko.update_brand_organizations")  # noqa
-def update_brand_organizations():
+def update_brand_organizations() -> None:
+    """
+    Update organizations of all brands
+    :return:
+    """
+
     for brand in IIKOBrand.objects.active():
         with atomic():
             brand.deactivate_organizations()
             GetOrganizations(instance=brand).run()
+
+
+@celery_app.task(name="iiko.update_brand_nomenclatures") # noqa
+def update_brand_nomenclatures():
+    """
+    Update nomenclatures of all brands
+    :return:
+    """
+
+    for organization in Organization.objects.active():
+        GetOrganizationNomenclature(instance=organization).run()
