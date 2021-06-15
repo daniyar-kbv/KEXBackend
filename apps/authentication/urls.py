327f98ac9ca174439b7ec3c191096e943cb5e06c
voyager1 <adilbekk8@gmail.com>
2021-06-15 15:51:46 +0600

    minor fixes in auth urls

--- apps/authentication/urls.py
+++ apps/authentication/urls.py
@@ -5,7 +5,7 @@ from .views import RegisterAccountView, VerifyAccountView, OTPResendView
 
 urlpatterns = [
     path("register/", RegisterAccountView.as_view()),
-    path("otp_verify/", VerifyAccountView.as_view(), name="otp_verify_view"),
-    path("otp_resend/", OTPResendView.as_view(), name="otp_resend_view"),
+    path("otp-verify/", VerifyAccountView.as_view(), name="otp_verify_view"),
+    path("otp-resend/", OTPResendView.as_view(), name="otp_resend_view"),
     path("refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
