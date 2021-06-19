02a4dcefbf6d89b6947ba7777903bb07a6670a0a
voyager1 <adilbekk8@gmail.com>
2021-06-19 15:08:29 +0600

    minor fixes in users view

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,21 +1,18 @@
-# from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.generics import ListAPIView
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.generics import RetrieveAPIView
 from rest_framework.generics import GenericAPIView
 from rest_framework.response import Response
 
 from apps.common.mixins import JSONRendererMixin
 
-from .models import User
 from .serializers import (
     AccountInfoSerializer,
 )
 
 
-class AccountInfoView(JSONRendererMixin, GenericAPIView):
+class AccountInfoView(JSONRendererMixin, RetrieveAPIView):
     serializer_class = AccountInfoSerializer
 
-    def get(self, request, *args, **kwargs):
+    def retrieve(self, request, *args, **kwargs):
         serializer = self.serializer_class(instance=request.user)
         return Response(serializer.data)
 
