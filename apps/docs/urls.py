1b9287e49ae81dd915f14b3750ba3b9e7d648b8b
voyager1 <adilbekk8@gmail.com>
2021-06-24 08:43:56 +0600

    uncomment docs

--- apps/docs/urls.py
+++ apps/docs/urls.py
@@ -4,6 +4,6 @@ from .views import DocumentView, DocumentListView, ContactListView
 
 urlpatterns = [
     path("", DocumentListView.as_view()),
-    # path("contacts/", ContactListView.as_view()),
+    path("contacts/", ContactListView.as_view()),
     path("<str:document_slug>/", DocumentView.as_view(), name="render-by-slug"),
 ]
