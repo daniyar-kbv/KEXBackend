7c7820d2eeceeda571534be18f9dcc728b7e6c63
voyager1 <adilbekk8@gmail.com>
2021-07-18 20:22:40 +0600

    Payments: add '/' in confirm-payment api path

--- apps/payments/urls.py
+++ apps/payments/urls.py
@@ -11,5 +11,5 @@ urlpatterns = [
     path("my-cards/", DebitCardsListView.as_view()),
     path("create-payment/", CreatePaymentView.as_view()),
     path("create-card-payment/", CreateCardPaymentView.as_view()),
-    path("confirm-payment/<uuid:payment_uuid>", Confirm3DSPaymentView.as_view()),
+    path("confirm-payment/<uuid:payment_uuid>/", Confirm3DSPaymentView.as_view()),
 ]
