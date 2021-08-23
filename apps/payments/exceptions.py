b73cdc9f236b959bb4c92f7aff07fed240fba64d
voyager1 <adilbekk8@gmail.com>
2021-08-24 02:17:03 +0600

    Payments: add change_for for cash payments

--- apps/payments/exceptions.py
+++ apps/payments/exceptions.py
@@ -1,8 +1,13 @@
 from rest_framework.exceptions import APIException
 
-from config.constants.error_codes import ORDER_ALREADY_PAID
+from config.constants.error_codes import ORDER_ALREADY_PAID, CHANGE_FOR_NOT_SET
 
 
 class OrderAlreadyPaidError(APIException):
     status_code = 400
     default_code = ORDER_ALREADY_PAID
+
+
+class ChangeForNotSetError(APIException):
+    status_code = 400
+    default_code = CHANGE_FOR_NOT_SET
