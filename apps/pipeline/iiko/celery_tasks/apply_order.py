fce6a857bbe02e45ec356d1cfe43b30a1a5b5d30
voyager1 <adilbekk8@gmail.com>
2021-09-30 17:21:31 +0600

    minor fixes in apply_delivery_order

--- apps/pipeline/iiko/celery_tasks/apply_order.py
+++ apps/pipeline/iiko/celery_tasks/apply_order.py
@@ -7,11 +7,7 @@ from ..integrations.apply_order import ApplyDeliveryOrder
 
 @celery_app.task(name='iiko.apply_delivery_order')
 def apply_delivery_order(order_pk: int):
-    order = Order.objects\
-        .prefetch_related(
-            "cart__position",
-            "cart__position__modifiers",
-        ).get(id=order_pk)
+    order = Order.objects.get(id=order_pk)
 
     return ApplyDeliveryOrder(
         instance=order
