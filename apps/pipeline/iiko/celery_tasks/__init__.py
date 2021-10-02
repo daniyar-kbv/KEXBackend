38b3d4816daf2922b8decf975dd5d824b0b6ed4c
voyager1 <adilbekk8@gmail.com>
2021-10-02 22:17:37 +0600

    Orders: add tasks import

--- apps/pipeline/iiko/celery_tasks/__init__.py
+++ apps/pipeline/iiko/celery_tasks/__init__.py
@@ -2,3 +2,4 @@ from .beat_tasks import update_brand_branches
 from .branches import find_lead_organization
 from .auth import fetch_auth_token
 from .apply_order import order_apply_task
+from .update_status import update_order_status
