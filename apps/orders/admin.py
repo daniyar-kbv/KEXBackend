a4d6f8b82a30ff0e129268552b4877cf186f8505
voyager1 <adilbekk8@gmail.com>
2021-10-02 20:22:48 +0600

    Orders: minor changes in admin page

--- apps/orders/admin.py
+++ apps/orders/admin.py
@@ -1,15 +1,26 @@
 from django.contrib import admin
 
-from apps.common.admin import HistoryInline, AbstractTitleModelForm, AbstractDescriptionModelForm, AbstractNameModelForm
+from apps.common.admin import HistoryInline, ReadOnlyMixin
 
-from .models import Lead, Cart
-from .models.orders import Order
+from .models import Lead, Cart, Order, OrderStatusTransition
+
+
+class OrderStatusTransitionInline(ReadOnlyMixin, admin.TabularInline):
+    model = OrderStatusTransition
+    extra = 0
+    classes = ('collapse',)
+    fields = ('status', 'status_reason', 'created_at')
+    readonly_fields = ('status', 'status_reason', 'created_at')
 
 
 class LeadAdmin(admin.ModelAdmin):
     inlines = (HistoryInline,)
 
 
+class OrderAdmin(ReadOnlyMixin, admin.ModelAdmin):
+    inlines = (HistoryInline, OrderStatusTransitionInline)
+
+
 admin.site.register(Lead, LeadAdmin)
-admin.site.register(Order)
+admin.site.register(Order, OrderAdmin)
 admin.site.register(Cart)
