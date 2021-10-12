d59e8ee0c81010b00f502009c74df1bdc676545e
voyager1 <adilbekk8@gmail.com>
2021-10-12 09:48:14 +0600

    IIKO: minor fixes in beat tasks

--- apps/pipeline/iiko/celery_tasks/beat_tasks.py
+++ apps/pipeline/iiko/celery_tasks/beat_tasks.py
@@ -9,7 +9,7 @@ from ..integrations.terminals import GetLocalBrandTerminals, CheckLocalBrandOrga
 
 @celery_app.task(name="iiko.update_brand_organizations")  # noqa
 def update_brand_branches() -> None:
-    for local_brand in LocalBrand.objects.active():
+    for local_brand in LocalBrand.objects.all():
         with atomic():
             local_brand.deactivate_branches()
             GetBranches(instance=local_brand).run()
@@ -19,7 +19,7 @@ def update_brand_branches() -> None:
 
 @celery_app.task(name="iiko.update_brand_nomenclatures")  # noqa
 def update_local_brands_nomenclatures():
-    for local_brand in LocalBrand.objects.active():
+    for local_brand in LocalBrand.objects.all():
         with atomic():
             GetLocalBrandNomenclature(instance=local_brand).run()
             for branch in local_brand.branches.all():
