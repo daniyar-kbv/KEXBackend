d9d43311a7db89bab672484b431a8bb95172c970
voyager1 <adilbekk8@gmail.com>
2021-10-15 12:45:26 +0600

    Pipeline: add new service status

--- apps/pipeline/__init__.py
+++ apps/pipeline/__init__.py
@@ -10,4 +10,5 @@ class ServiceStatuses(TextChoices):
     NO_REQUEST = "NO_REQUEST", _('Не было запроса')
     WAS_REQUEST = "WAS_REQUEST", _('Был запрос')
     REQUEST_ERROR = "REQUEST_ERROR", _('Ошибка запроса')
+    UNAUTHORIZED = "UNAUTHORIZED", _('Ошибка авторизации')
     SERVICE_UNAVAILABLE = "SERVICE_UNAVAILABLE", _('Сервис не доступен')
