a80136b5fda55851b3cfb4b069cac45dc36777d0
voyager1 <adilbekk8@gmail.com>
2021-10-25 23:05:42 +0600

    Celery: add gevent worker

--- config/celery_app.py
+++ config/celery_app.py
@@ -1,10 +1,16 @@
 import os
 from celery import Celery
+from kombu import Exchange, Queue
 
 if not ("DJANGO_SETTINGS_MODULE" in os.environ):
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.development')
 
 app = Celery("kex")
+
+app.conf.task_queues = [
+    Queue("celery", Exchange("celery"), routing_key="celery"),
+    Queue("celery-gevent", Exchange('celery-gevent'), routing_key="celery-gevent"),
+]
 
 app.config_from_object("django.conf:settings", namespace="CELERY")
 
