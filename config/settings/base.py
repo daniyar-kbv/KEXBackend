774a4f5805526a7882fc49f40835d04861487b25
voyager1 <adilbekk8@gmail.com>
2021-10-26 00:47:26 +0600

    Add throttling

--- config/settings/base.py
+++ config/settings/base.py
@@ -263,6 +263,14 @@ REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 100,
     'COERCE_DECIMAL_TO_STRING': False,
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle'
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '100/minute',
+        'user': '100/minute'
+    }
 }
 
 SIMPLE_JWT = {
