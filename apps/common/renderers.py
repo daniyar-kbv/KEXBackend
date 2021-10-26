163da2c8d71231ca5fc20cf6b2f8727f617b1a6d
voyager1 <adilbekk8@gmail.com>
2021-10-27 04:19:15 +0600

    add headers logging

--- apps/common/renderers.py
+++ apps/common/renderers.py
@@ -5,10 +5,11 @@ from .response_handler.services import execute_handler
 
 class JSONRenderer(DefaultJSONRenderer):
     def render(self, data, accepted_media_type=None, renderer_context=None):
+        print('headers:', renderer_context['request'].headers)
         formatted_response = execute_handler(
             data=data,
             raw_response=renderer_context['response'],
-            language=renderer_context["request"].headers.get("Language")
+            language=renderer_context["request"].headers.get("Language"),
         )
 
         return super(JSONRenderer, self).render(formatted_response, accepted_media_type, renderer_context)
