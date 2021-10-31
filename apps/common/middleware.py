84d80cf8c36a1e4e13f0ca501b5f1fc5a0a49cd2
voyager1 <adilbekk8@gmail.com>
2021-10-31 19:31:51 +0600

    minor fix in UserAgentMiddleware

--- apps/common/middleware.py
+++ apps/common/middleware.py
@@ -1,4 +1,5 @@
 from constance import config
+from django.http import JsonResponse
 
 from config.constants import user_agents_mobile
 
@@ -6,9 +7,11 @@ from .exceptions import IOSNotAvailableError, AndroidNotAvailableError
 
 
 class UserAgent(object):
-    user_agent_string = None
-    is_mobile = None
-    is_pc = None
+    user_agent_string: str = None
+    is_mobile: bool = None
+    is_pc: bool = None
+    error_code: str = None
+    error_message: str = ""
 
     def __init__(self, uastring):
         self.user_agent_string = uastring
@@ -16,11 +19,14 @@ class UserAgent(object):
         self.is_mobile = False
 
         if uastring == 'IOS' and not config.IOS_ON:
-            print('raised IOS NOT AVAILABLE ERORR')
-            raise IOSNotAvailableError
+            self.error_code = IOSNotAvailableError.default_code
+            if hasattr(config, f"{self.error_code}_ru"):
+                self.error_message = getattr(config, f"{self.error_code}_ru")
 
         if uastring == 'Android' and not config.ANDROID_ON:
-            raise AndroidNotAvailableError
+            self.error_code = AndroidNotAvailableError.default_code
+            if hasattr(config, f"{self.error_code}_ru"):
+                self.error_message = getattr(config, f"{self.error_code}_ru")
 
         if uastring:
             for uword in user_agents_mobile:
@@ -41,6 +47,14 @@ class UserAgentMiddleware(object):
 
     def process_request(self, request):
         user_agent = request.META.get('HTTP_USER_AGENT')
-        setattr(request, 'user_agent', UserAgent(user_agent))
+        user_agent_instance = UserAgent(user_agent)
+        setattr(request, 'user_agent', user_agent_instance)
+
+        if user_agent_instance.error_code:
+            return JsonResponse({"data": None, "error": {
+                "code": user_agent_instance.error_code,
+                "message": user_agent_instance.error_message,
+            }})
+
         response = self.get_response(request)
         return response
