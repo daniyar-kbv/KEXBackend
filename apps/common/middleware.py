8edf9eeae0dc7ffec6fae8181947a87bcf03ee24
voyager1 <adilbekk8@gmail.com>
2021-10-31 19:18:33 +0600

    add IOS, Android control

--- apps/common/middleware.py
+++ apps/common/middleware.py
@@ -1,4 +1,8 @@
+from constance import config
+
 from config.constants import user_agents_mobile
+
+from .exceptions import IOSNotAvailableError, AndroidNotAvailableError
 
 
 class UserAgent(object):
@@ -7,10 +11,16 @@ class UserAgent(object):
     is_pc = None
 
     def __init__(self, uastring):
-        print('uastring', uastring)
         self.user_agent_string = uastring
         self.is_pc = False
         self.is_mobile = False
+
+        if uastring == 'IOS' and not config.IOS_ON:
+            print('raised IOS NOT AVAILABLE ERORR')
+            raise IOSNotAvailableError
+
+        if uastring == 'Android' and not config.ANDROID_ON:
+            raise AndroidNotAvailableError
 
         if uastring:
             for uword in user_agents_mobile:
