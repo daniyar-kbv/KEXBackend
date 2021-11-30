36113888cd1a92f5924762113b7e86cf0e853858
voyager1 <adilbekk8@gmail.com>
2021-11-30 10:07:05 +0600

    Minor fix in BranchDeliveryTime

--- apps/partners/models.py
+++ apps/partners/models.py
@@ -223,7 +223,7 @@ class BranchDeliveryTime(models.Model):
         choices=DeliveryTypes.choices,
     )
     zone_name = models.CharField(
-        max_length=256, editable=False
+        max_length=256
     )
     start_time = models.TimeField(_("Время работы с"), default=time(10, 0))
     end_time = models.TimeField(_("Время работы до"), default=time(22, 0))
